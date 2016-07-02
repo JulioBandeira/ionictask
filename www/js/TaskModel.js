6875720d474c74aab0c992e87eb2e00e8f0e6428
JulioBandeira <info.bandeira@gmail.com>
2016-07-02 09:37:38 -0400

    primeiro commit

---
+++ www/js/TaskModel.js
@@ -0,0 +1,6 @@
+function GetTarefas(params) {
+    this.itens = [
+             {nome:"Julio Bandeira", opcao:false}, 
+             {nome:'Marcelo', opcao:false}, 
+             {nome:'Hermes', opcao:false}];
+}
